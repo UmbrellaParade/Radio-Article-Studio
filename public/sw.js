2ff0c0923bf59bd9ccf09c7fcfd45102e1de91e1
UmbrellaParade <bellbo13@users.noreply.github.com>
2026-07-12 19:39:56 +0900

    Bump service worker cache version

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "radio-article-studio-v7";
+const CACHE_VERSION = "radio-article-studio-v8";
 const BASE_PATH = new URL(self.registration.scope).pathname;
 const APP_SHELL = [
   BASE_PATH,
